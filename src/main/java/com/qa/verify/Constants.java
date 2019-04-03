75569136e88e406f07640377f9ffd72196e68bea
RMSGroupQA <48963771+RMSGroupQA@users.noreply.github.com>
2019-04-03 13:53:27 +0100

    Upload for test live environment

---
+++ src/main/java/com/qa/verify/Constants.java
@@ -0,0 +1,7 @@
+package com.qa.verify;
+
+public class Constants {	
+	
+	public final static String CREATEEMPLOYEE= "http://51.141.6.150:8080/emailVerify/getters/createEmployee";
+	
+}
---
+++ src/main/java/com/qa/verify/controller/EmailVerifyController.java
@@ -0,0 +1,34 @@
+package com.qa.verify.controller;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.qa.verify.Constants;
+import com.qa.verify.object.Employee;
+import com.qa.verify.service.EndWithVerifyService;
+
+@RestController
+public class EmailVerifyController {
+
+	private EndWithVerifyService service;
+	private RestTemplate restTemplate;
+
+	public EmailVerifyController(EndWithVerifyService service, RestTemplateBuilder builder) {
+		this.service = service;
+		this.restTemplate = builder.build();
+	}
+
+	@CrossOrigin
+	@PostMapping("/emailVerify/createEmployee")
+	public String verify(@RequestBody Employee employee) {
+		if (service.endWithVerify(employee) != null) {
+			return restTemplate.postForEntity(Constants.CREATEEMPLOYEE, employee, String.class).getBody();
+		}
+		return "Invalid email.";
+	}
+
+}
