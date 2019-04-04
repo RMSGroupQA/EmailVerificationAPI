6bce5e77a8bbc5f30760e701528d883c009bb1d5
RMSGroupQA <48963771+RMSGroupQA@users.noreply.github.com>
2019-04-04 09:53:45 +0100

    Upload for test live environment
    ports changed

--- src/main/java/com/qa/verify/Constants.java
+++ src/main/java/com/qa/verify/Constants.java
@@ -2,6 +2,6 @@ package com.qa.verify;
 
 public class Constants {	
 	
-	public final static String CREATEEMPLOYEE= "http://51.141.6.150:8080/emailVerify/getters/createEmployee";
+	public final static String CREATEEMPLOYEE= "http://51.141.6.150:8082/emailVerify/getters/createEmployee";
 	
 }
